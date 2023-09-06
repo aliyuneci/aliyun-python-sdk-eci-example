02fa01518615adfa372ad2ab661ac46250be11a2
wb549296 <wb-hxh549296@alibaba-inc.com>
2023-09-06 16:02:29 +0800

    创建数据缓存样例

---
+++ eci/data_cache/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 Aliyun.com All right reserved. This software is the
+# confidential and proprietary information of Aliyun.com ("Confidential
+# Information"). You shall not disclose such Confidential Information and shall
+# use it only in accordance with the terms of the license agreement you entered
+# into with Aliyun.com .
+
+# created by xiaohui at 2023/9/6 15:43
--- eci/volumes/configFile.py
+++ eci/volumes/configFile.py
@@ -51,4 +51,4 @@ def createContainerGroup():
 
     response = client.do_action_with_exception(request)
 
-    print response
+    print(response)
